533078a1074b1bba746b2f40399a7cc721b37ec3
Raphael Tholl <raphael.tholl@ibm.com>
2023-08-16 14:28:39 +0200

    added newline in response

--- sample-app/routes/api/v1.js
+++ sample-app/routes/api/v1.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-res.send(`Hello my version is: ${process.env.VERSION || "v1"}`);
+res.send(`Hello my version is: ${process.env.VERSION || "v1"}\n`);
 });
 
 
