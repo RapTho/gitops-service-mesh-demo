5c7cb5a12fe0798999d0fcec382480fbca20f87c
Raphael Tholl <raphael.tholl@ibm.com>
2023-08-16 13:40:33 +0200

    changed message

--- sample-app/routes/api/v1.js
+++ sample-app/routes/api/v1.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-res.send(`Hello my role is: ${process.env.ROLE || "DEFAULT"}`);
+res.send(`Hello my version is: ${process.env.VERSION || "v1"}`);
 });
 
 
