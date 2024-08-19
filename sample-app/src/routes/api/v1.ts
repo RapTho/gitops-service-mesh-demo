f5b6e682baee24c202612259d9ddf1ab40dd3f89
Raphael Tholl <raphael.tholl@ibm.com>
2024-08-19 15:39:07 +0200

    add failing endpoint

--- sample-app/src/routes/api/v1.ts
+++ sample-app/src/routes/api/v1.ts
@@ -1,12 +1,24 @@
 import express, { Request, Response } from 'express';
 const router = express.Router();
 
+let counter = 0;
+
 router.get("/", (req: Request, res: Response) => {
     res.send(`Hello my version is: ${process.env.VERSION || "v1"}\n`);
 });
 
 router.post("/echo", (req: Request, res: Response) => {
     res.send(`The client sent: ${JSON.stringify(req.body)}\n`);
+})
+
+router.get("/fail", (req: Request, res: Response) => {
+    if(counter <4) {
+        counter++;
+        res.sendStatus(503);
+    } else {
+        counter = 0;
+        res.sendStatus(200);
+    }
 })
 
 module.exports = router;
